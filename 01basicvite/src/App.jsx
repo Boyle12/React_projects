53663e1f164c57e6e5274b9b9409cd0aff8c39b4
ABHINAV KAUSHAL <abhinavkaushaldos5988@gmail.com>
2026-05-29 23:16:02 +0530

    Added 01basicvite folder

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,15 @@
+import Youtube from "./Youtube"
+
+function App() {
+  const username = 'Aakash'
+  return (
+    <>
+      <h1>vite react app{username}</h1>
+      <h1>vite react app{2*3}</h1>
+      <Youtube/>
+    </>
+    
+  )
+}
+
+export default App
